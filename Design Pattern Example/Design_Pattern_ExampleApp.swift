a2ad24f80fdfb23bceecf94b74333f8b9877438a
KhuePM <minhkhuepham99@gmail.com>
2024-10-03 21:18:18 +0700

    Initial Commit

---
+++ Design Pattern Example/Design_Pattern_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Design_Pattern_ExampleApp.swift
+//  Design Pattern Example
+//
+//  Created by KhuePM on 03/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Design_Pattern_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
